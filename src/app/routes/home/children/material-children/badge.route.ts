c484f7df822ceeb885d56701a16afe118adfe488
Jaime Still <jpstill85@gmail.com>
2022-12-09 20:17:34 -0500

    badge + button routes

--- src/app/routes/home/children/material-children/badge.route.ts
+++ src/app/routes/home/children/material-children/badge.route.ts
@@ -1,7 +1,45 @@
+import {
+    MatBadgePosition,
+    MatBadgeSize
+} from '@angular/material/badge';
+
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
     selector: 'badge-route',
-    templateUrl: 'badge.route.html'
+    templateUrl: 'badge.route.html',
+    styleUrls: ['badge.route.scss']
 })
-export class BadgeRoute { }
+export class BadgeRoute {
+    colors: ThemePalette[] = [
+        'primary',
+        'accent',
+        'warn'
+    ];
+
+    positions: MatBadgePosition[] = [
+        'above',
+        'below',
+        'before',
+        'after',
+        'above before',
+        'above after',
+        'below before',
+        'below after'
+    ];
+
+    sizes: MatBadgeSize[] = [
+        'large',
+        'medium',
+        'small'
+    ];
+
+    color: ThemePalette = 'primary';
+    content: string | number | undefined | null = 1;
+    disabled: boolean = false;
+    hidden: boolean = false;
+    overlap: boolean = true;
+    position: MatBadgePosition = 'above after';
+    size: MatBadgeSize = 'medium';
+}
