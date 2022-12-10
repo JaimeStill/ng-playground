00e28df6345e06c1eb5c1e75d2b5c82ff3e800ed
Jaime Still <jpstill85@gmail.com>
2022-12-09 21:19:46 -0500

    checkbox route

--- src/app/routes/home/children/material-children/checkbox.route.ts
+++ src/app/routes/home/children/material-children/checkbox.route.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
     selector: 'checkbox-route',
-    templateUrl: 'checkbox.route.html'
+    templateUrl: 'checkbox.route.html',
+    styleUrls: ['checkbox.route.scss']
 })
-export class CheckboxRoute { }
+export class CheckboxRoute {
+    positions: string[] = [
+        'before',
+        'after'
+    ];
+
+    colors: ThemePalette[] = [
+        'primary',
+        'accent',
+        'warn'
+    ];
+
+    checked: boolean = false;
+    indeterminate: boolean = false;
+    disabled: boolean = false;
+    color: ThemePalette = 'primary';
+    position: 'before' | 'after' = 'before';
+}
