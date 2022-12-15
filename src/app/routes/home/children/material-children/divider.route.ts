837798ef3b1f529334c66a41cefa0866fd93b9cb
Jaime Still <jpstill85@gmail.com>
2022-12-15 16:21:35 +0000

    divider route

--- src/app/routes/home/children/material-children/divider.route.ts
+++ src/app/routes/home/children/material-children/divider.route.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { FxDirection } from '../../../../types/flex';
 
 @Component({
     selector: 'divider-route',
     templateUrl: 'divider.route.html'
 })
-export class DividerRoute { }
+export class DividerRoute {
+    inset: boolean = false;
+    vertical: boolean = false;
+
+    direction = (): FxDirection => this.vertical
+        ? 'row'
+        : 'column';
+}
