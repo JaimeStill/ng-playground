dee257b97c6ad86ee4a01df9044c15e3b8cfe7e9
Jaime Still <jpstill85@gmail.com>
2022-12-15 13:52:40 -0500

    form-field route

--- src/app/routes/home/children/material-children/form-field.route.ts
+++ src/app/routes/home/children/material-children/form-field.route.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { FloatLabelType, MatFormFieldAppearance } from '@angular/material/form-field';
 
 @Component({
     selector: 'form-field-route',
-    templateUrl: 'form-field.route.html'
+    templateUrl: 'form-field.route.html',
+    styleUrls: ['form-field.route.scss']
 })
-export class FormFieldRoute { }
+export class FormFieldRoute {
+    appearance: MatFormFieldAppearance = 'fill';
+    color: ThemePalette = 'primary';
+    floatLabel: FloatLabelType = 'auto';
+    hintAlign: 'start' | 'end' = 'start';
+
+    hide: boolean = true;
+}
