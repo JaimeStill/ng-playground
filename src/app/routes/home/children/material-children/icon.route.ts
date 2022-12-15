8f96627fb69007267433cc5a97daa4d7e43b5f88
Jaime Still <jpstill85@gmail.com>
2022-12-15 18:25:33 -0500

    icon route

--- src/app/routes/home/children/material-children/icon.route.ts
+++ src/app/routes/home/children/material-children/icon.route.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
     selector: 'icon-route',
-    templateUrl: 'icon.route.html'
+    templateUrl: 'icon.route.html',
+    styleUrls: ['icon.route.scss']
 })
-export class IconRoute { }
+export class IconRoute {
+    matColor: ThemePalette | undefined = undefined;
+    matIcon: string = 'explore';
+    matSize: number = 126;
+    matClass: 'material-icons' | 'material-icons-outlined' = 'material-icons';
+
+    faColor: ThemePalette | undefined = undefined;
+    faSet: 'fas' | 'far' | 'fab' = 'fab';
+    faIcon: string = 'fa-github';
+    faLabel: string = 'GitHub';
+}
