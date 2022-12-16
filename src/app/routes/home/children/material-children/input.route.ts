9b8991f328d7d948b6fc6e1931e2c1c8dd8f168b
Jaime Still <jpstill85@gmail.com>
2022-12-15 19:30:51 -0500

    input route

--- src/app/routes/home/children/material-children/input.route.ts
+++ src/app/routes/home/children/material-children/input.route.ts
@@ -2,6 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'input-route',
-    templateUrl: 'input.route.html'
+    templateUrl: 'input.route.html',
+    styleUrls: ['input.route.scss']
 })
-export class InputRoute { }
+export class InputRoute {
+    inputs = {
+        color: '#00c853',
+        date: '2022-12-25',
+        localDatetime: '2022-12-25T18:00',
+        email: 'robots@demo.net',
+        month: '2022-12',
+        number: 333,
+        password: 'P@$$W0rd123456',
+        search: 'How do I Google?',
+        phone: '555-444-1234',
+        text: 'So many input types!',
+        time: '18:00',
+        url: 'https://mapoftheuniverse.net/'
+    }
+
+    openUrl = (url: string) => window.open(url, '_blank');
+}
