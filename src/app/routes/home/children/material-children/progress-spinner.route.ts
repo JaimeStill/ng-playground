abd7a5eb56a39db1566f844b8375845e63708cf3
Jaime Still <jpstill85@gmail.com>
2022-12-16 12:28:03 -0500

    progress-spinner route

--- src/app/routes/home/children/material-children/progress-spinner.route.ts
+++ src/app/routes/home/children/material-children/progress-spinner.route.ts
@@ -1,7 +1,39 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    OnDestroy,
+    OnInit
+} from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
+
+import {
+    interval,
+    Subscription
+} from 'rxjs';
 
 @Component({
     selector: 'progress-spinner-route',
     templateUrl: 'progress-spinner.route.html'
 })
-export class ProgressSpinnerRoute { }
+export class ProgressSpinnerRoute implements OnInit, OnDestroy {
+    color: ThemePalette = 'primary';
+    mode: ProgressSpinnerMode = 'determinate';
+    strokeWidth: number = 10;
+    diameter: number = 200;
+
+    value: number = 0;
+    valueSub: Subscription;
+
+    ngOnInit(): void {
+        this.valueSub = interval(80)
+            .subscribe(() =>
+                this.value = this.value < 100
+                    ? this.value + 2
+                    : 0
+            );
+    }
+
+    ngOnDestroy(): void {
+        this.valueSub?.unsubscribe();
+    }
+}
