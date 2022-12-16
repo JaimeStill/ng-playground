f8cfa2495a884df7a16adfd63bdf8d28034336e2
Jaime Still <jpstill85@gmail.com>
2022-12-16 09:59:57 -0500

    paginator route

--- src/app/routes/home/children/material-children/paginator.route.ts
+++ src/app/routes/home/children/material-children/paginator.route.ts
@@ -1,7 +1,33 @@
 import { Component } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
     selector: 'paginator-route',
-    templateUrl: 'paginator.route.html'
+    templateUrl: 'paginator.route.html',
+    styleUrls: ['paginator.route.scss']
 })
-export class PaginatorRoute { }
+export class PaginatorRoute {
+    length: number = 50;
+    pageSize: number = 10;
+    pageIndex: number = 0;
+    pageSizeOptions = [5, 10, 25];
+
+    hidePageSize = false;
+    showPageSizeOptions = true;
+    showFirstLastButtons = true;
+    disabled: false;
+
+    pageEvent: PageEvent;
+
+    handlePageEvent = (e: PageEvent) => {
+        this.pageEvent = e;
+        this.length = e.length;
+        this.pageSize = e.pageSize;
+        this.pageIndex = e.pageIndex;
+    }
+
+    setPageSizeOptions = (options: string) => {
+        if (options)
+            this.pageSizeOptions = options.split(',').map(str => +str);
+    }
+}
