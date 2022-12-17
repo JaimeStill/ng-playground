a6d342404ba17e339db602933f727cac2fcfe31f
Jaime Still <jpstill85@gmail.com>
2022-12-16 20:40:35 -0500

    expansion route

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {
   MatCommonModule,
   MatLineModule,
+  MatNativeDateModule,
   MatOptionModule,
   MatPseudoCheckboxModule,
   MatRippleModule
@@ -20,6 +21,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -54,6 +56,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatCheckboxModule,
     MatChipsModule,
     MatCommonModule,
+    MatDatepickerModule,
     MatDialogModule,
     MatDividerModule,
     MatExpansionModule,
@@ -64,6 +67,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatLineModule,
     MatListModule,
     MatMenuModule,
+    MatNativeDateModule,
     MatOptionModule,
     MatPaginatorModule,
     MatProgressBarModule,
--- src/app/routes/home/children/material-children/expansion.route.ts
+++ src/app/routes/home/children/material-children/expansion.route.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'expansion-route',
-    templateUrl: 'expansion.route.html'
+    templateUrl: 'expansion.route.html',
+    styleUrls: ['expansion.route.scss']
 })
-export class ExpansionRoute { }
+export class ExpansionRoute {
+    expanded: boolean = false;
+
+    step = 0;
+
+    setStep = (index: number) => this.step = index;
+
+    nextStep = () => this.step++;
+
+    prevStep = () => this.step--;
+}
